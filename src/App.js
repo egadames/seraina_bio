366927fb8c99b7a87b2abedddc7a80f40ca46969
egadames <egadames990@gmail.com>
2021-06-22 22:22:38 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+} from "react-router-dom";
+import Homepage from './pages/home';
+// import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import SignUp from '../SignUp';
+// import SignIn from '../SignIn';
+// import SignOut from '../SignOut';
+
+// import Navbar from './../../components/Navbar';
+// import Homepage from  "../../components/Homepage";
+// import newTeam from '../newTeam';
+// import Teams from '../Team'
+// import "./style.css";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Route exact path='/' component={Homepage} />
+      </Router>
+    )
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       {/* <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header> */}
+//     </div>
+//   );
+// }
 
 export default App;
---
+++ src/pages/home.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Homepage extends Component {
+  render(){
+    return(
+      <div className='homapgecontainer ui grid'>
+        <h1>TEST</h1>
+      </div>
+    )
+  }
+
+}
+
+export default Homepage;
