0e245618d59c1be614f9578eaa5512269dbabd49
egadames <egadames990@gmail.com>
2021-06-22 22:37:28 -0700

    updated linter for js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
   Route,
-} from "react-router-dom";
+} from 'react-router-dom';
 import Homepage from './pages/home';
 // import { connect } from 'react-redux';
 
@@ -12,10 +12,10 @@ import Homepage from './pages/home';
 // import SignOut from '../SignOut';
 
 // import Navbar from './../../components/Navbar';
-// import Homepage from  "../../components/Homepage";
+// import Homepage from  '../../components/Homepage';
 // import newTeam from '../newTeam';
 // import Teams from '../Team'
-// import "./style.css";
+// import './style.css';
 
 class App extends Component {
   render() {
@@ -23,22 +23,22 @@ class App extends Component {
       <Router>
         <Route exact path='/' component={Homepage} />
       </Router>
-    )
+    );
   }
 }
 // function App() {
 //   return (
-//     <div className="App">
-//       {/* <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
+//     <div className='App'>
+//       {/* <header className='App-header'>
+//         <img src={logo} className='App-logo' alt='logo' />
 //         <p>
 //           Edit <code>src/App.js</code> and save to reload.
 //         </p>
 //         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
+//           className='App-link'
+//           href='https://reactjs.org'
+//           target='_blank'
+//           rel='noopener noreferrer'
 //         >
 //           Learn React
 //         </a>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 
 class Homepage extends Component {
-  render(){
-    return(
+  render() {
+    return (
       <div className='homapgecontainer ui grid'>
         <h1>TEST</h1>
       </div>
@@ -12,3 +12,11 @@ class Homepage extends Component {
 }
 
 export default Homepage;
+
+    // "lint-staged": {
+    //   "src/**/*.{js,jsx,ts,tsx,json,css,scss,md}": [
+    //     "prettier --write"
+    //   ]
+    // },
+
+        // "prepare": "husky install"
